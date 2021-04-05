5fc300f38719172d5c12cb67047cc776724a9c61
Raúl Acosta <raulacostaservicio@hotmail.com>
2021-04-05 14:44:37 -0500

    Añado React a mi currículum

---
+++ js/Script1.js
@@ -0,0 +1,26 @@
+console.log(`Todo bien`);
+
+$(document).ready(function () {
+    console.log('Testing');
+    
+    $('#DiseñoWebSección').animate({
+        height: '22vh'
+    },1500);
+
+    $('#ProgramaciónWebSección').animate({
+        height: '22vh'
+    },2500);
+
+    $('#VersionesSección').animate({
+        height: '22vh'
+    },3500);
+
+    $('#BaseDatosSección').animate({
+        height: '22vh'
+    },4500);
+
+    $('#BackEndSección').animate({
+        height: '22vh'
+    },5500);
+    
+});
